f447194db47613fe9fc6f860540ede82fadd9837
Artem Voskresensky <metravs@gmail.com>
2023-11-20 23:35:48 +0300

    add minigrep

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,12 +1,17 @@
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-    }
+    use super::*;
 
     #[test]
-    fn another() {
-        panic!("Make this test fail");
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
